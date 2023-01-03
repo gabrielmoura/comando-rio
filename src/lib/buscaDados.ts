03e67b3fb39749b93319b59667205984e5615909
Gabriel Moura <gabriel.blx32@gmail.com>
2023-01-03 13:36:10 -0300

    Atualização da API 2023.

--- src/lib/buscaDados.ts
+++ src/lib/buscaDados.ts
@@ -1,15 +1,16 @@
-import Api from './Api';
 import atualizaDados from './atualizaDados';
 import {write} from "./writeFile";
+import axios from "axios";
+import {format, subDays} from "date-fns";
 
 export default async () => {
-    const api = new Api(process.env.USERAPI, process.env.PASSAPI);
-    await api.autorization();
-
-    await api.getData(data => {
-        // Escrita em arquivo
-        write(data);
-        // Escrita no banco de dados
-        data.eventos.map(evento => atualizaDados(evento));
-    });
+    axios.get('https://api.dados.rio/v2/adm_cor_comando/ocorrencias', {
+        params: {
+            format: 'json',
+            inicio: format(subDays(new Date(), 29), 'yyyy-MM-dd 00:00:00.0')
+        }
+    }).then(resp => {
+        write(resp.data);
+        resp.data.eventos.map(evento => atualizaDados(evento));
+    })
 }
